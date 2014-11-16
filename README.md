6522213dc8d9a005b8f56ef2d81591f73ec766e1
H. Cole Wiley <hcwiley@gmail.com>
2014-11-16 16:24:26 -0600

    forgot to add a couple things

---
+++ README.md
@@ -0,0 +1,4 @@
+django_art
+=========
+
+Django CMS for artist 
---
+++ portfolio/portfolio/artist/migrations/0002_auto__add_field_artist_tagline.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Artist.tagline'
+        db.add_column(u'artist_artist', 'tagline',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=255, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Artist.tagline'
+        db.delete_column(u'artist_artist', 'tagline')
+
+
+    models = {
+        u'artist.artist': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Artist'},
+            'artist_statement': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'bio': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'cover_photo': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'cover_photo'", 'null': 'True', 'to': u"orm['artist.ParentMedia']"}),
+            'cv': ('django.db.models.fields.files.FileField', [], {'default': "''", 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'head_shot': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['artist.ParentMedia']", 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'slug': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'tagline': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'}),
+            'website': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        u'artist.artistmedia': {
+            'Meta': {'ordering': "('position',)", 'object_name': 'ArtistMedia', '_ormbases': [u'artist.ParentMedia']},
+            'aux_images': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'aux_images'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['artist.ParentMedia']"}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['artist.ArtistMediaCategory']"}),
+            'description': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'dimensions': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'medium': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            u'parentmedia_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['artist.ParentMedia']", 'unique': 'True', 'primary_key': 'True'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1'}),
+            'year': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '4', 'null': 'True', 'blank': 'True'})
+        },
+        u'artist.artistmediacategory': {
+            'Meta': {'ordering': "['position']", 'object_name': 'ArtistMediaCategory'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1'}),
+            'slug': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        u'artist.link': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'Link'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100'}),
+            'url': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        u'artist.parentmedia': {
+            'Meta': {'ordering': "['name']", 'object_name': 'ParentMedia'},
+            'full_res_image': ('django.db.models.fields.files.ImageField', [], {'default': "''", 'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'default': "''", 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'is_default_image': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100'}),
+            'thumbnail': ('django.db.models.fields.files.ImageField', [], {'default': "''", 'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'video_link': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        u'artist.press': {
+            'Meta': {'ordering': "['-date']", 'object_name': 'Press'},
+            'content': ('tinymce.models.HTMLField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateField', [], {'default': 'datetime.date.today'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_archived': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100'})
+        },
+        u'artist.presslink': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'PressLink', '_ormbases': [u'artist.Link']},
+            u'link_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['artist.Link']", 'unique': 'True', 'primary_key': 'True'}),
+            'press_article': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['artist.Press']"})
+        },
+        u'artist.pressmedia': {
+            'Meta': {'ordering': "['name']", 'object_name': 'PressMedia', '_ormbases': [u'artist.ParentMedia']},
+            u'parentmedia_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['artist.ParentMedia']", 'unique': 'True', 'primary_key': 'True'}),
+            'press_article': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['artist.Press']"})
+        },
+        u'artist.show': {
+            'Meta': {'ordering': "('-date',)", 'object_name': 'Show'},
+            'date': ('django.db.models.fields.DateField', [], {'default': 'datetime.date.today'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'link': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['artist.Link']", 'null': 'True', 'blank': 'True'}),
+            'location': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100'})
+        },
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['artist']
